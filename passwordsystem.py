f7908a6e53d693080c3c3a9717f54f3edc0f54de
SamTheWarlock <43090976+SamTheWarlock@users.noreply.github.com>
2020-02-06 19:44:30 +0000

    Update passwordsystem.py

--- passwordsystem.py
+++ passwordsystem.py
@@ -1,67 +1,67 @@
 def redoAO():
-    return("Perform an action:")
-    return("1. Change a password.")
-    return("2. Delete an account")
-    return("3. Log Out")
+    print("Perform an action:")
+    print("1. Change a password.")
+    print("2. Delete an account")
+    print("3. Log Out")
     inp = input("What would you like to do? Enter a number: ")
     if inp == "1":
         changeacc = input("Which account would you like to modify the password on?: ")
         if changeacc == logins.a[0]:
-            return("To what?: ")
+            print("To what?: ")
             passwordin = input("Enter a password: ")
             logins.a[1] = passwordin
-            return("Password Set.")
+            print("Password Set.")
             redoAO()
         elif changeacc == logins.c[0]:
-            return("To what?: ")
+            print("To what?: ")
             passwordin = input("Enter a password: ")
             logins.c[1] = passwordin
-            return("Password Set.")
+            print("Password Set.")
             redoAO()
         elif changeacc == logins.d[0]:
-            return("To what?: ")
+            print("To what?: ")
             passwordin = input("Enter a password: ")
             logins.d[1] = passwordin
-            return("Password Set.")
+            print("Password Set.")
             redoAO()
         elif changeacc == logins.e[0]:
-            return("To what?: ")
+            print("To what?: ")
             passwordin = input("Enter a password: ")
             logins.e[1] = passwordin
-            return("Password Set.")
+            print("Password Set.")
             redoAO()
         elif changeacc == logins.f[0]:
-            return("To what?: ")
+            print("To what?: ")
             passwordin = input("Enter a password: ")
             logins.f[1] = passwordin
-            return("Password Set.")
+            print("Password Set.")
             redoAO()
         elif changeacc == logins.b[0]:
-            return("You cannot change the password of an actively logged in user.")
+            print("You cannot change the password of an actively logged in user.")
             redoAO()
         else:
-            return("User undefined.")
+            print("User undefined.")
             redoAO()
     elif inp == "2":
         who = input("Who would you like to delete?: ")
         if who == logins.a[0]:
-            return("Are you sure? Press the enter key to confirm.")
+            print("Are you sure? Press the enter key to confirm.")
             input(">>")
             logins.a[0] = False
             logins.a[1] = False
-            return("Account Deleted. (Re-run the IDLE to re-allow access)")
+            print("Account Deleted. (Re-run the IDLE to re-allow access)")
             redoAO()
         elif who == logins.b[0]:
-            return("Admin user is un-deletable.")
+            print("Admin user is un-deletable.")
             redoAO()
         else:
-            return("User undefined.")
+            print("User undefined.")
             redoAO()
     elif inp == "3":
-        return("Successfully logged out as Admin.")
+        print("Successfully logged out as Admin.")
         retry()
     else:
-        return("Action undefined.")
+        print("Action undefined.")
         redoAO()
             
 
@@ -79,57 +79,57 @@ def retry():
     if name.lower() == logins.a[0]:
          pasw = input("Please enter your password: ")
          if pasw == logins.a[1]:
-            return("Welcome!")
+            print("Welcome!")
          else:
-            return("Incorrect password.")
+            print("Incorrect password.")
             retry()
     elif name.lower() == logins.b[0]:
-        return("Welcome,",name)
+        print("Welcome,",name)
         pasw = input("Please enter your password: ")
         if pasw == logins.b[1]:
-            return("Welcome")
+            print("Welcome")
             adminloggedin = True
             redoAO()
         else:
-            return("Incorrect password.")
+            print("Incorrect password.")
             retry()
     elif name.lower() == logins.c[0]:
-        return("Welcome,",name)
+        print("Welcome,",name)
         pasw = input("Please enter your password: ")
         if pasw == logins.c[1]:
-            return("Welcome")
-            return("ERR: Local Admin disabled by global administrator")
+            print("Welcome")
+            print("ERR: Local Admin disabled by global administrator")
         else:
-            return("Incorrect password.")
+            print("Incorrect password.")
             retry()
     elif name.lower() == logins.d[0]:
-        return("Welcome,",name)
+        print("Welcome,",name)
         pasw = input("Please enter your password: ")
         if pasw == logins.d[1]:
-            return("Why did I make you admin what no")
+            print("Why did I make you admin what no")
             redoAO()
         else:
-            return("Incorrect password.")
+            print("Incorrect password.")
             
             retry()
     elif name.lower() == logins.e[0]:
-        return("Welcome,",name)
+        print("Welcome,",name)
         pasw = input("Please enter your password: ")
         if pasw == logins.e[1]:
-            return("Welcome")
+            print("Welcome")
         else:
-            return("Incorrect password.")
+            print("Incorrect password.")
             retry()
     elif name.lower() == logins.f[0]:
-        return("Welcome,",name)
+        print("Welcome,",name)
         pasw = input("Please enter your password: ")
         if pasw == logins.f[1]:
-            return("Welcome")
+            print("Welcome")
         else:
-            return("Incorrect password.")
+            print("Incorrect password.")
             retry()
     else:
-        return("Sorry. Your username is invalid.")
+        print("Sorry. Your username is invalid.")
         retry()
 
 retry()
